753c4b67d63f5cd9e9d1efd4cd5710689685b5f6
REM_ <ultimate.spidey.18@gmail.com>
2018-02-26 13:00:20 -0600

    Revert "Implemented Better Click Handler System"
    This reverts commit b1b66c7eb8cca0108d0e87ebd87d5130f75eef64.

--- CHEF_DOLORES_REVAMP_UNITY_PROJECT/Assets/_REM_CHEF_DOLORES_PACKAGE/Scripts/ClickHandler.cs
+++ CHEF_DOLORES_REVAMP_UNITY_PROJECT/Assets/_REM_CHEF_DOLORES_PACKAGE/Scripts/ClickHandler.cs
@@ -10,39 +10,23 @@ public class ClickHandler : MonoBehaviour {
 		instance = this;
 	}
 
-	public void CheckClickProperties(){
+	void CheckClickProperties(){
 	
 		if (Input.GetMouseButtonDown(0)) {
-			//Debug.Log ("Player has clicked on something.... checkingClickProperties method...");
-			bool hasItem = IsHoldingItem ();
-
-
+			//Checks if there is an object in sight
 			if (RemCaster.instance.obj_in_sight != null) {
-				//Debug.Log ("Player has clicked on something");
+				Debug.Log ("Player has clicked on something");
 				GameObject clicked = RemCaster.instance.obj_in_sight;
 
 				switch (clicked.tag) {
 				//CASES
 					case "ingredient":
-					//Debug.Log ("Player has clicked on ingredient; Object in hand=" + hasItem.ToString());
-						
-						//IF HAS NO ITEM IN HAND GRAB ITEM
-						if (hasItem == false) {
-						PlayerHand.instance.HoldItem (clicked);
-						}
-
 					break;
 
 					case "trash":
-					Debug.Log ("Player has clicked on trash; Object in hand=" + hasItem.ToString());
 					break;
 
-					case "MixerMachine":
-					//Debug.Log ("Player has clicked on MixerMachine; Object in hand=" + hasItem.ToString());
-						//IF HAS ITEM; PLACE IT ON MACHINE
-					if (hasItem == true) {
-						StartCoroutine (DropItemInsideMachine ());
-						}
+					case "machine":
 					break;
 
 					case "FinishedDish":
@@ -56,30 +40,10 @@ public class ClickHandler : MonoBehaviour {
 
 
 					default:
-					Debug.Log ("Clickin on something else!!!");
 					break;
 				}
 
 			}
 		}
-	}
-
-	public bool IsHoldingItem(){
-		//Checks if there is an item in hand
-		bool boolValue;
-		if (PlayerHand.instance.held_item != null) {
-			//Debug.Log ("item in hand = " + true);
-			boolValue = true;
-		} else {
-			//Debug.Log ("item in hand = " + false);
-			boolValue = false;
-		}
-		return boolValue;
-	}
-
-	IEnumerator DropItemInsideMachine(){
-		PlayerHand.instance.DropItem ();
-		yield return new WaitForSeconds (0.5f);
-		//MixerMachine.instance.ReceiveIngredient (PlayerHand.instance.held_item);
 	}
 }
