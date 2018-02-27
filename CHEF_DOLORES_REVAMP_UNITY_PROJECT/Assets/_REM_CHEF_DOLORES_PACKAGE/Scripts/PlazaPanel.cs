17d1474b299efbb8ea19ee3d406cd8d97d9603bd
REM_ <ultimate.spidey.18@gmail.com>
2018-02-27 10:56:36 -0600

    Populate Buttons Works
    Added Plaza Panel
    - Grabs a super string, separates it and populates the panel with real time buttons, Im awesome

--- CHEF_DOLORES_REVAMP_UNITY_PROJECT/Assets/_REM_CHEF_DOLORES_PACKAGE/Scripts/PlazaPanel.cs
+++ CHEF_DOLORES_REVAMP_UNITY_PROJECT/Assets/_REM_CHEF_DOLORES_PACKAGE/Scripts/PlazaPanel.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlazaPanel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public string testString;
+
+	public string[] plazas;
+	public string[] separators;
+	public GameObject buttonPrefab;
+	public RectTransform buttonsParent;
+
+	public void GetPlazaButtons(string longString, string[] separators){
 	
-	// Update is called once per frame
-	void Update () {
-		
+		//separates the long string based on selected separators and stores them inside plazas array
+		plazas = longString.Split (separators, System.StringSplitOptions.RemoveEmptyEntries);
+
+		//FOR EACH ELEMENT INSIDE PLAZAS ARRAY ->
+		for (int i = 0; i < plazas.Length; i++) {
+			//Creates a prefab and palce is inside the panel
+			GameObject instantiatedButton = Instantiate (buttonPrefab, buttonsParent.transform) as GameObject;
+			Button button = instantiatedButton.GetComponent<Button> ();
+			//changes the name and label
+			instantiatedButton.name = "Button_" + plazas [i];
+			button.GetComponentInChildren<Text> ().text = plazas [i];
+		}
+	}
+
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.P)) {
+			GetPlazaButtons (testString, separators);
+			Debug.Log ("Getting plaza buttons");
+		}
 	}
 }
